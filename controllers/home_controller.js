3fdf044438435227bb36755f67d04717f48fe312
ANANDM113 <anandmaheshwari113@gmail.com>
2023-08-01 16:58:10 +0530

    Creating Home.ejs and rendering from the corresponding controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,11 @@
 //module.exports will export (home) action outside. we will importing it in route
 
 module.exports.home =   function(request,response){
-    return response.end('<h1>Express is up for Codeial!</h1>');
+    //direct rendering it
+    //return response.end('<h1>Express is up for Codeial!</h1>');
+
+    //rendering the View(home.ejs)
+    return response.render('home',{
+        title: "Home"
+    });
 }
