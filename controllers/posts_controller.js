e4dba48f11ed9fcda1ee84a8a79cf1ee878755a9
ANANDM113 <anandmaheshwari113@gmail.com>
2023-08-30 12:34:27 +0530

    deleting post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post  =   require('../models/post');
+const Comment   =   require('../models/comment');
 
 module.exports.create   =   function(request,response){
     Post.create({
@@ -12,5 +13,24 @@ module.exports.create   =   function(request,response){
     })
     .catch((err) =>{
         console.log('Error in Posting');
+    })
+}
+
+module.exports.destroy  =   function(request,response){
+    //checking whether the post exist or not
+    Post.findById(request.params.id)
+    .then((post) => {
+
+        //We are checking only that user should be able to delete the post who has written it
+        // .id means converting the object is ._id into string
+        if(post.user == request.user.id){
+            post.deleteOne();
+            Comment.deleteMany({post: request.params.id})
+            .then(() =>{
+                return response.redirect('back');
+            })
+        }else{
+            return response.redirect('back');
+        }
     })
 }
